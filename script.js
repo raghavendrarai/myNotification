7ec3a27900d9e781c5f5ab1e8faa3a1d23fc3a1d
Raghavvendra Rai K <raghavendrarai.kadar@gmail.com>
2016-12-07 11:45:39 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,71 @@
+(function doesBrowserSupportNotifications() {
+    var supported = true;
+    if (!('showNotification' in ServiceWorkerRegistration.prototype)) {
+        console.warn('Notificationsn\'t supported in Service Workers.');
+        supported = false;
+    }
+
+    if (!Notification.requestPermission) {
+        console.warn("Notificationsnot supported by the browser");
+        supported = false;
+    }
+
+    // Check if push messaging is supported  
+    if (!('PushManager' in window)) {
+        console.warn('Pushsaging isn\'t supported.');
+        supported = false;
+    }
+
+    if(supported) {
+        console.log("Everthingine you can continue")
+    }
+})();
+
+///////////////////////
+ if (navigator.serviceWorker) {
+   console.log("ServiceWorkerssupported");
+
+   navigator.serviceWorker.register('sw.js', {
+     scope: './'
+   })
+   .then(function(reg) {
+     console.log("ServiceWorkerstered", reg);
+   })
+   .catch(function(error) {
+     console.log("Failedegister ServiceWorker", error);
+   });
+}
+
+//////////////////////
+
+function requestNotificationPermission() {
+    if (Notification.requestPermission) {
+        Notification.requestPermission(function(result) {
+            console.log("Notificationission : ", result);
+        });
+    } else {
+        console.log("Notificationssupported by this browser.");
+    }
+}
+
+/////////////////////////////
+
+function registerForPush() {
+    if (navigator.serviceWorker.controller) {
+        navigator.serviceWorker.ready.then(function(serviceWorkerRegistration) {
+            serviceWorkerRegistration.pushManager.subscribe({
+                    userVisibleOnly: true
+                })
+                .then(function(subscription) {
+                    console.log("SubscriptionPush successful: ", subscription.endpoint);
+                })
+                .catch(function(error) {
+                    console.log("SubscriptionPush failed", error);
+                });
+        });
+    } else {
+        console.log("Nove ServiceWorker");
+    }
+}
+
+/////////////////////////////
